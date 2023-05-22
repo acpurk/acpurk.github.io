2c3a47aac294e7d6dd46bb5218f8e85f0b4ec054
acpurk <ohblah@hotmail.com>
2023-05-23 01:44:25 +0800

    Create matchingmechanism.js

---
+++ matchingmechanism.js
@@ -0,0 +1,31 @@
+// Set up the request options
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer YOUR_AUTH_TOKEN'
+  },
+  body: JSON.stringify({
+    inputConfig: {
+      gcsSource: {
+        inputUris: ['gs://YOUR_BUCKET_NAME/YOUR_FILE_NAME.csv']
+      },
+      dataType: 'CSV'
+    },
+    outputConfig: {
+      gcsDestination: {
+        outputUriPrefix: 'gs://YOUR_BUCKET_NAME/YOUR_OUTPUT_FOLDER'
+      }
+    },
+    model: 'projects/YOUR_PROJECT_ID/locations/YOUR_LOCATION_ID/models/YOUR_MODEL_ID',
+    params: {
+      scoreThreshold: '0.5'
+    }
+  })
+};
+
+// Make the API request
+fetch('https://automl.googleapis.com/v1beta1/YOUR_NAME:batchPredict', options)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
